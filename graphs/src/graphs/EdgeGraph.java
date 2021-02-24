23f3d438452f11db8fddd27b08241a86784518a0
Veronika Kitsul <65815639+Veronika-Kitsul@users.noreply.github.com>
2021-02-23 21:14:27 -0500

    final

--- graphs/src/graphs/EdgeGraph.java
+++ graphs/src/graphs/EdgeGraph.java
@@ -9,16 +9,19 @@ public class EdgeGraph<E, T>
 {
 	HashMap<E, Vertex> vertices;
 	
+	// add vertices to a hash map that matches their input to the actual vertex
 	public EdgeGraph()
 	{
 		vertices = new HashMap<E, Vertex>();
 	}
 	
+	// constructor for adding vertices
 	public void addVertex(E info)
 	{
 		vertices.put(info, new Vertex(info));
 	}
 	
+	// connecting vertices with edges
 	public void connect(E info1, E info2, T label)
 	{
 		Vertex v1 = vertices.get(info1);
@@ -30,6 +33,7 @@ public class EdgeGraph<E, T>
 		v2.edges.add(e);
 	}
 	
+	// constructor of labeled edges
 	public class Edge
 	{
 		T label;
@@ -52,6 +56,7 @@ public class EdgeGraph<E, T>
 		}
 	}
 	
+	// constructor of vertices
 	private class Vertex 
 	{
 		E info;
@@ -69,8 +74,10 @@ public class EdgeGraph<E, T>
 		}
 	}
 	
+	// BFS algorithm
 	public ArrayList<Object> search(E place, E destiny)
 	{
+		// handle null pointers and give details as to what went wrong
 		if (vertices.get(destiny) == null)
 		{
 			System.out.println("There is no destination with this name. ");
@@ -82,27 +89,35 @@ public class EdgeGraph<E, T>
 			return null;
 		}
 		
+		// create a list of places to visit
 		ArrayList<Vertex> toVisit = new ArrayList<Vertex>();
 		toVisit.add(vertices.get(place));
 		
+		// create a list of visited places so we don't waste time visiting them twice
 		HashSet<Vertex> visited = new HashSet<Vertex>();
 		visited.add(vertices.get(place));
 		
+		// create a map to remember which vertex leads to which vertex
 		HashMap<Vertex, Edge> leadsTo = new HashMap<Vertex, Edge>();
 		
-		
+		// while we have nothing to visit == we searched the whole graph
 		while (!toVisit.isEmpty())
 		{
+			// we don't need to visit current vertex anymore because we are here
 			Vertex curr = toVisit.remove(0);
 			
+			// for all of the edges to the current vertex
 			for (Edge e: curr.edges)
 			{
 				Vertex neighbor = e.getNeighbor(curr);
 				
+				// if you have already visited then just continue
 				if (visited.contains(neighbor)) continue;
 				
+				// put the path to the map
 				leadsTo.put(neighbor, e);
 				
+				// if we found what we are looking for then call backTrace to trace all the path back to the start
 				if (neighbor.info.equals(destiny))
 				{
 					return backTrace(neighbor, leadsTo);
@@ -117,13 +132,18 @@ public class EdgeGraph<E, T>
 		return null;
 	}
 	
+	// back trace method
 	private ArrayList<Object> backTrace(Vertex destiny, HashMap<Vertex, Edge> leadsTo)
 	{
+		// start at the final position
 		Vertex curr = destiny;
+		// array list to hold the path
 		ArrayList<Object> path = new ArrayList<Object>();
 		
+		// while there is a vertex to lead us to another vertex
 		while (leadsTo.get(curr) != null) 
 		{
+			// add it to the path
 			path.add(0, curr.info);
 			path.add(0, leadsTo.get(curr).label);
 			curr = leadsTo.get(curr).getNeighbor(curr);
@@ -132,14 +152,17 @@ public class EdgeGraph<E, T>
 		return path;
 	}
 	
+	// farthest method to find farthest connection
 	public ArrayList<Object> farthest(E info) 
 	{
+		// handle null pointer
 		if (vertices.get(info) == null)
 		{
 			System.out.println("There is no destination with this name. ");
 			return null;
 		}
 
+		// similar procedure to the BFS -- refer to the search method above
 		ArrayList<Vertex> toVisit = new ArrayList<Vertex>();
 		toVisit.add(vertices.get(info));
 		
@@ -149,6 +172,8 @@ public class EdgeGraph<E, T>
 		HashMap<Vertex, Edge> leadsTo = new HashMap<Vertex, Edge>();
 		
 		Vertex curr = toVisit.get(0);
+		
+		// search while searched the whole graph
 		while (!toVisit.isEmpty())
 		{			
 			curr = toVisit.remove(0);
@@ -161,6 +186,9 @@ public class EdgeGraph<E, T>
 				
 				leadsTo.put(neighbor, e);
 				
+				// here we don't have a condition to check if it's the destination yet because we don't know the destination
+				// just search the whole graph and make connections
+				
 				toVisit.add(neighbor);
 				visited.add(neighbor);
 			}
@@ -168,10 +196,10 @@ public class EdgeGraph<E, T>
 		return backTrace(curr, leadsTo);
 	}
 	
-// working on this now ------------------------	
+	// method to find three shortest ways to get to the destination
 	public ArrayList<ArrayList<Object>> threeWays(E start, E end) 
 	{
-
+		// handle null pointers
 		if (vertices.get(end) == null)
 		{
 			System.out.println("There is no destination with this name. ");
@@ -183,6 +211,7 @@ public class EdgeGraph<E, T>
 			return null;
 		}
 		
+		// similar code to search and farthest -- see details on it above
 		ArrayList<Vertex> toVisit = new ArrayList<Vertex>();
 		toVisit.add(vertices.get(start));
 		
@@ -205,9 +234,12 @@ public class EdgeGraph<E, T>
 				
 				leadsTo.put(neighbor, e);
 				
+				// search for ways the way you searched in the search method
 				if (neighbor.info.equals(end))
 				{
 					 ways.add(backTrace(neighbor, leadsTo));
+					 
+					 // if you reached the needed number of paths -- return them
 					 if (ways.size() == 3)
 					 {
 						 return ways;
@@ -226,18 +258,5 @@ public class EdgeGraph<E, T>
 	public static void main(String[] args)
 	{
 		EdgeGraph<String, String> g = new EdgeGraph<String, String>();
-		g.addVertex("R");
-		g.addVertex("A");
-		g.addVertex("K");
-		g.addVertex("H");
-		g.addVertex("B");
-		g.addVertex("C");
-		
-		g.connect("K", "A", "food");
-		g.connect("R", "A", "sports");
-		g.connect("R", "H", "shops");
-		g.connect("H", "C", "chocolate");
-		g.connect("C", "B", "tennis");
-		g.threeWays("R", "C");
 	}
 }
--- graphs/src/graphs/KevinBaconGame.java
+++ graphs/src/graphs/KevinBaconGame.java
@@ -33,21 +33,23 @@ public class KevinBaconGame
 	HashMap<String, String> codeMovie = new HashMap<String, String>();
 	
 	
-	public KevinBaconGame() throws IOException 
+	public KevinBaconGame() 
 	{
 		// creating readers and file variables for all files
 		File actors = new File("actors.txt");
 		File movies = new File("movies.txt");
 		File movieActors = new File("movie-actors.txt");
-		BufferedReader actorsReader = new BufferedReader(new FileReader(actors));
+		BufferedReader actorsReader;
+		try {
+			actorsReader = new BufferedReader(new FileReader(actors));
+		
 		BufferedReader moviesReader = new BufferedReader(new FileReader(movies));
 		BufferedReader moviesActors = new BufferedReader(new FileReader(movieActors));
-		
-		//FileReader moviesActors = new FileReader(movieActors);
 		
 		//creating a graph
 		EdgeGraph<String, String> graph = new EdgeGraph<String, String>();
 		
+		// looping through the file of actors and putting them into a hash map, adding them to the graph as vertices
 		int k = 0;
 		for (String line = actorsReader.readLine(); line != null; line = actorsReader.readLine())
 		{
@@ -58,32 +60,40 @@ public class KevinBaconGame
 		}
 		actorsReader.close();
 		
+		// looping through the file of movies and putting them into the hash map
 		for (String line = moviesReader.readLine(); line != null; line = moviesReader.readLine())
 		{
 			String[] array = line.split("~");
 			codeMovie.put(array[0], array[1]);
 		}
 		
-		
+		// looping through the file of movies to actors codes
+		// setting keys so I know what is the previous key 
 		String prevKey = "";
 		String key = "";
 		int connections = 0;
+		
+		// an array list to keep all codes of actors from the same movie
 		ArrayList<String> codes = new ArrayList<String>();
 		for (String line = moviesActors.readLine(); line != null; line = moviesActors.readLine())
 		{
 			String[] array = line.split("~");
 			key = array[0];
 			
+			// if previous key is the same as current or if this is a first time looping add actor to the array list
 			if(prevKey.equals(key)|| prevKey == "")
 			{
 				codes.add(array[1]);
 			}
+			// if they are not equal and this is not the first line
 			else if (prevKey.equals(key) == false && prevKey != "")
 			{
+				// loop through the array list and connect each element in it with another elements through nested loops
 				for (int j = 0; j < codes.size(); j++)
 				{
 					for (int n = j + 1; n < codes.size(); n++)
 					{
+						// do not make another connection between the object itself
 						if (j == n)
 						{
 							
@@ -95,13 +105,17 @@ public class KevinBaconGame
 						}
 					}
 			    }
+				// clear the array list for the next movie
 				codes.clear();
-				
 			}
+			// update keys for the net movie
 			prevKey = key;
 			key = "";
 		}
 		
+	// GUI ----------
+		
+		// create frames and panels, add panels to the frame, and set a title
 		JFrame frame = new JFrame();
 		JPanel panel = new JPanel();
 		JPanel top = new JPanel();
@@ -115,7 +129,7 @@ public class KevinBaconGame
 		BoxLayout boxlayout = new BoxLayout(panel, BoxLayout.Y_AXIS);
 		panel.setLayout(boxlayout);
 		
-		// actor 1 text field
+		// actor 1 text field and label 
 		JTextArea actor1 = new JTextArea();
 		actor1.setEditable(true);
 		actor1.setPreferredSize(new Dimension(100, 20));
@@ -123,7 +137,7 @@ public class KevinBaconGame
 		top.add(one);
 		top.add(actor1);
 		
-		// actor 2 text field
+		// actor 2 text field and label
 		JTextArea actor2 = new JTextArea();
 		actor2.setEditable(true);
 		actor2.setPreferredSize(new Dimension(100, 20));
@@ -131,179 +145,225 @@ public class KevinBaconGame
 		top.add(two);
 		top.add(actor2);
 		
-		// display area to display results
-		JTextArea display = new JTextArea();
-		display.setEditable(false);
-		display.setPreferredSize(new Dimension(560, 400));
-		display.setLineWrap(true);
-        display.setWrapStyleWord(true);
-		top.add(display);
+		// display area to display results of operations
+			JTextArea display = new JTextArea();
+			display.setEditable(false);
+			display.setPreferredSize(new Dimension(560, 400));
+			display.setLineWrap(true);
+		
+			// if the text is too much to fit, wrap it and to the top panel
+	        display.setWrapStyleWord(true);
+			top.add(display);
 		
 		// search button to run BFS
-		JButton shortest = new JButton("Shortest path");
-		bottom.add(shortest);
-		
-		shortest.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent e) 
+			JButton shortest = new JButton("Shortest path");
+			bottom.add(shortest);
+			
+			// action listener to search button to run BFS
+			shortest.addActionListener(new ActionListener()
 			{
-				String first = actor1.getText().trim();
-				String second = actor2.getText().trim();
-				
-				if (first.equals(""))
+				public void actionPerformed(ActionEvent e) 
 				{
-					display.setText("Please input the first actor and run again. ");
-					return;
-				}
-				else if (second.equals(""))
-				{
-					display.setText("Please input the second actor and run again. ");
-					return;
-				}
-				
-				if (graph.search(first, second) == null)
-				{
-					display.setText("Sorry, there is no information you are looking for. You might want to check if actors' names are correct, or we might just not have them in our database. ");
-				}
-				else
-				{
-					display.setText((graph.search(first, second).toString()));
-				}
-			}
-		});
-		
-		// farthest button to run farthest method
-		JButton farthest = new JButton("Farthest actor");
-		bottom.add(farthest);
-		
-		farthest.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent e) 
-			{
-				String first = actor1.getText().trim();
-				
-				if (first.equals(""))
-				{
-					display.setText("Please input the first actor and run again. ");
-					return;
-				}
-				
-				if (graph.farthest(first) == null)
-				{
-					display.setText("Sorry, there is no information you are looking for. You might want to check if actors' names are correct, or we might just not have them in our database. ");
-				}
-				else
-				{
-					display.setText((graph.farthest(first)).toString());
-				}
-			}
-		});
-		
-		JButton ways = new JButton("3 Shortest Ways");
-		bottom.add(ways);
-		
-		ways.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent e) 
-			{
-				String first = actor1.getText().trim();
-				String second = actor2.getText().trim();
-				
-				if (first.equals(""))
-				{
-					display.setText("Please input the first actor and run again. ");
-					return;
-				}
-				else if (second.equals(""))
-				{
-					display.setText("Please input the second actor and run again. ");
-					return;
-				}
-				
-				if (graph.threeWays(first, second) == null)
-				{
-					display.setText("Sorry, there is no information you are looking for. You might want to check if actors' names are correct, or we might just not have them in our database. ");
-				}
-				else 
-				{
-					graph.threeWays(first, second);
-					String output = "1) ";
+					// take the input from the user
+					String first = actor1.getText().trim();
+					String second = actor2.getText().trim();
 					
-					ArrayList<ArrayList<Object>> ways = graph.threeWays(first, second);
-					output = output + ways.get(0) + "\n\n" + "2) " + ways.get(1) + "\n\n" + "3) " + ways.get(2);
-		
-					display.setText(output);
+					// if there is nothing in the input -- give a message
+					if (first.equals(""))
+					{
+						display.setText("Please input the first actor and run again. ");
+						return;
+					}
+					else if (second.equals(""))
+					{
+						display.setText("Please input the second actor and run again. ");
+						return;
+					}
+					
+					// if the user is trying to input two same names
+					if (first.equals(second))
+					{
+						display.setText("You search a path from the actor to oneself, are you sure that this is what you meant? You are already in your destined place.");
+					}
+					// if there is no path or no information found
+					else if (graph.search(first, second) == null)
+					{
+						display.setText("Sorry, there is no information you are looking for. You might want to check if actors' names are correct, or we might just not have them in our database. ");
+					}
+					// else just return a result of the operation
+					else
+					{
+						display.setText((graph.search(first, second).toString()));
+					}
 				}
-			}
-		});
+			});
+		
+		// farthest button to run farthest method that returns farthest actor to actor1
+			JButton farthest = new JButton("Farthest actor");
+			bottom.add(farthest);
+			
+			farthest.addActionListener(new ActionListener()
+			{
+				public void actionPerformed(ActionEvent e) 
+				{
+					// get the first actor because we work with it only
+					String first = actor1.getText().trim();
+					
+					// if there is no input
+					if (first.equals(""))
+					{
+						display.setText("Please input the first actor and run again. ");
+						return;
+					}
+					
+					// if there is no path (handle null pointers)
+					if (graph.farthest(first) == null)
+					{
+						display.setText("Sorry, there is no information you are looking for. You might want to check if actors' names are correct, or we might just not have them in our database. ");
+					}
+					// else return the result of the operation
+					else
+					{
+						display.setText((graph.farthest(first)).toString());
+					}
+				}
+			});
+			
+		// ways button to display 3 shortest ways from one actor to another
+			JButton ways = new JButton("3 Shortest Ways");
+			bottom.add(ways);
+			
+			ways.addActionListener(new ActionListener()
+			{
+				public void actionPerformed(ActionEvent e) 
+				{
+					// take the input from the user
+					String first = actor1.getText().trim();
+					String second = actor2.getText().trim();
+					
+					// if there is no input for any of the fields
+					if (first.equals(""))
+					{
+						display.setText("Please input the first actor and run again. ");
+						return;
+					}
+					else if (second.equals(""))
+					{
+						display.setText("Please input the second actor and run again. ");
+						return;
+					}
+					
+					// if actors are equal there is no sense to run search
+					if (first.equals(second))
+					{
+						display.setText("You search a path from the actor to oneself, are you sure that this is what you meant? You are already in your destined place.");
+					}
+					// handle null pointer if there is no path
+					else if (graph.threeWays(first, second) == null)
+					{
+						display.setText("Sorry, there is no information you are looking for. You might want to check if actors' names are correct, or we might just not have them in our database. ");
+					}
+					// else return the result of the operation
+					else 
+					{
+						// nice formatting of the output
+						graph.threeWays(first, second);
+						String output = "1) ";
+						
+						ArrayList<ArrayList<Object>> ways = graph.threeWays(first, second);
+						output = output + ways.get(0) + "\n\n" + "2) " + ways.get(1) + "\n\n" + "3) " + ways.get(2);
+			
+						display.setText(output);
+					}
+				}
+			});
 		
 		
 		// creating a button to switch between themes
-		JButton theme = new JButton("Swtich theme");
-		bottom.add(theme);
-		
-		// getting the original colors of panels to use it in light mode later
-		Color panelcolor = panel.getBackground();
-		Color bottomColor = bottom.getBackground();
-				
-		// if you click on the dark mode button, it will set backgrounds of buttons panel and overall panel to dark palette
-		theme.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent e) 
+			JButton theme = new JButton("Switch theme");
+			bottom.add(theme);
+			
+			// getting the original colors of panels to use it in the light mode later
+			Color panelcolor = panel.getBackground();
+			Color bottomColor = bottom.getBackground();
+			
+			// action listener for when you press a button
+			theme.addActionListener(new ActionListener()
 			{
-				// if it's black -- go for white
-				if (panel.getBackground().equals(new Color (90, 130, 100)))
+				public void actionPerformed(ActionEvent e) 
 				{
-					bottom.setBackground(bottomColor);
-					panel.setBackground(panelcolor);
-					display.setBackground(Color.white);
-					top.setBackground(panelcolor);
+					// if it's black -- go for white
+					if (panel.getBackground().equals(new Color (135, 170, 185)))
+					{
+						bottom.setBackground(bottomColor);
+						panel.setBackground(panelcolor);
+						display.setBackground(Color.white);
+						top.setBackground(panelcolor);
+					}
+					// if it's not black -- set it to black
+					else 
+					{
+						// play around with colors and elements
+						bottom.setBackground(new Color (98, 120, 127));
+						top.setBackground(new Color (135, 170, 185));
+						display.setBackground(new Color(249, 243, 197));
+						panel.setBackground(new Color (135, 170, 185));
+					}
 				}
-				else 
-				{
-					bottom.setBackground(new Color (88, 110, 117));
-					top.setBackground(new Color (90, 130, 100));
-					display.setBackground(new Color(220, 220, 220));
-					panel.setBackground(new Color (90, 130, 100));
-				}
-			}
-		});
+			});
 		
-		JButton rules = new JButton("Rules and Instructions");
-		bottom.add(rules);
-		rules.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent e) 
+		// a button to display how to use the game and what are the limitations
+			JButton rules = new JButton("Rules and Instructions");
+			bottom.add(rules);
+			
+			// action listener
+			rules.addActionListener(new ActionListener()
 			{
-				JOptionPane message = new JOptionPane();
-				message.showMessageDialog(frame, "Hello and welcome to the Kevin Bacon Game!\n" +
-				                                  "You have two text fields to enter two actors,\n" + 
-				                                  "and you can perform three operations on them.\n" + 
-				                                  "First,  you can find the shortest path from one\n" +
-				                                  "actor to another through movies they stage in.\n" +
-				                                  "For the second one, you only need one actor and\n" +
-				                                  "even if you input two, we will run the search for\n" +
-				                                  "for the farthest actor in the map for just the\n" +
-				                                  "first one. The third feature is to show three\n" +
-				                                  "shortest paths how to get from the first actor\n" +
-				                                  "to the second one. Names of actors have to be\n" +
-				                                  "their real names inputted correctly. The program\n" +
-				                                  "will ask you to try again if you inputted the name\n" +
-				                                  "incorrectly.", "Rules and Instructions", JOptionPane.INFORMATION_MESSAGE);
-			}
-		});
+				public void actionPerformed(ActionEvent e) 
+				{
+					// call a pop up informational window 
+					// i needed to split the text because everything i found online about changing the size of the popup 
+					// window did not work here
+					JOptionPane message = new JOptionPane();
+					message.showMessageDialog(frame, "Hello and welcome to the Kevin Bacon Game!\n" +
+					                                  "You have two text fields to enter two actors,\n" + 
+					                                  "and you can perform three operations on them.\n" + 
+					                                  "First,  you can find the shortest path from one\n" +
+					                                  "actor to another through movies they stage in.\n" +
+					                                  "For the second one, you only need one actor and\n" +
+					                                  "even if you input two, we will run the search for\n" +
+					                                  "for the farthest actor in the map for just the\n" +
+					                                  "first one. The third feature is to show three\n" +
+					                                  "shortest paths how to get from the first actor\n" +
+					                                  "to the second one. Names of actors have to be\n" +
+					                                  "their real names inputted correctly. The program\n" +
+					                                  "will ask you to try again if you inputted the name\n" +
+					                                  "incorrectly.", "Rules and Instructions", JOptionPane.INFORMATION_MESSAGE);
+				}
+			});
 		
-		
+		// frame settings
 		frame.setSize(600, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		frame.setLocationRelativeTo(null);
 		frame.setResizable(false);
 		frame.setFocusable(true);
-	// another issue is how do I make it case sensitive?
+		
+		} 
+		catch (FileNotFoundException e1) 
+		{
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} 
+		catch (IOException e1) 
+		{
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 }
 
-
+	// run a game!
 	public static void main(String[] args) throws IOException
 	{
 		new KevinBaconGame();
